5afaf253f3c13a306b8f99e277e80179eb94263a
jaziel <jaziel1974@gmail.com>
2023-11-01 19:06:16 -0300

    whatsapp webhooks

--- pages/api/whatsapp.js
+++ pages/api/whatsapp.js
@@ -1,5 +1,6 @@
 export default function handle(req, res) {
     const { method } = req;
+    const token = "EAAI29Ddo4GcBOxLX9DHjoglL1THlZBTyb4uSsGwJn5SpqZBzuSK27YZCr6sNhMAB47S7VIbKtHpZAjT5TO7MK3HLJZCQKNaOuL62OKcAAwEhAF58P8fD0NGNjsBbNyrNpqv3LoeGN8pq2MkyAPPvUrYiAKXma3HeF9RpLsgo0vbsAuFe8NEwDdgwU1BodKWoOQooOg3aza7OUy1t7gL12skJctIvkGifsVxoZD"
 
     if (method === 'GET') {
         let mode = req.query["hub.mode"];
@@ -16,5 +17,36 @@ export default function handle(req, res) {
             }
         }
         res.status(403).send('Method not allowed.');
+    }
+
+    if (method === 'POST') {
+        let bodyData = req.body;
+        if(bodyData.object) {
+            if(bodyData.entry && 
+                bodyData.entry[0].changes &&
+                bodyData.entry[0].changes[0].value.message &&
+                bodyData.entry[0].changes[0].value.message[0]
+                ) {
+                    let phoneId = bodyData.entry[0].changes[0].value.metadata.phone_number_id;
+                    let from = bodyData.entry[0].changes[0].value.messages[0].from;
+                    let msgBody = bodyData.entry[0].changes[0].value.messages[0].text.body;
+
+                    axios({
+                        method: 'POST',
+                        url:"https://graph.facebook.com/v13.0/"+phoneId+"message?access_token="+token,
+                        data:{
+                            messaging_product:"whatsapp",
+                            to:from,
+                            text:"Hi... I'm vmn"
+                        },
+                        headers:{
+                            "Content-Type": "application/json"
+                        }
+                    });
+                    res.sendStatus(200);
+                }else{
+                    res.sendStatus(404);
+                }
+        }
     }
 }
