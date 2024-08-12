c77c44b2ae00345628886fb1ef52adc8bf075537
jaziel1974 <jaziel1974@gmail.com>
2024-08-12 10:22:39 -0300

    login issue workaround

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,12 @@
 import Layout from "@/components/Layout";
-import { useSession } from "next-auth/react";
 
 export default function Home() {
-  const { data: session } = useSession();
   return <Layout>
     <div className="text-blue-900 flex justify-between">
       <h2>
-        Hello, <b>{session?.user?.name}</b>
+        Hello
       </h2>
       <div className="flex bg-gray-200 gap-1 text-black rounded-lg overflow-hidden">
-        <img src={session?.user?.image} alt="" className="w-6 h-6"></img>
-        <span className="py-1 px-2">
-          {session?.user?.name}
-        </span>
       </div>
     </div>
   </Layout>
